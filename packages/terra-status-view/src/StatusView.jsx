4708be48786a57d3d0744b4d426af96602c59d56
AF042410 <altamash.fakki@cerner.com>
2017-08-29 17:45:30 -0500

    basic structure for Status View

--- packages/terra-status-view/src/StatusView.jsx
+++ packages/terra-status-view/src/StatusView.jsx
@@ -6,25 +6,90 @@ import styles from './StatusView.scss';
 
 const cx = classNames.bind(styles);
 
+const statusTypes = ['clinical', 'no_data', 'no_result', 'not_authorised', 'sensitive_data', 'error', 'no_internet', 'error_loading', 'custom'];
+
 const propTypes = {
- /*
- * Content to be displayed as the name
- */
-  name: PropTypes.string,
+  /**
+    * The status view icon and heading will be decided as per the type value.
+    */
+  type: PropTypes.oneOf(statusTypes).isRequired,
+  /**
+    * The status view icon.
+    */
+  icon: PropTypes.element,
+  /**
+    * The title of the status view.
+    */
+  heading: PropTypes.string,
+   /**
+    * The subtext description of the status view.
+    */
+  subtext: PropTypes.string,
+   /**
+    * The element to be dispaly in the subtext content container.
+    */
+  subtextContent: PropTypes.element,
+   /**
+    * Whether or not the no Icon should be displayed.
+    */
+  isIconHidden: PropTypes.bool,
 };
 
 const defaultProps = {
-  name: 'default',
+  icon: undefined,
+  heading: undefined,
+  subtext: undefined,
+  subtextContent: undefined,
+  isIconHidden: false,
 };
 
-const StatusView = ({ name, ...customProps }) => {
+const StatusView = ({
+  type,
+  icon,
+  heading,
+  subtext,
+  subtextContent,
+  isIconHidden,
+  ...customProps }) => {
+  let iconSection;
+  if (!isIconHidden) {
+    iconSection = <div className={cx('icon')}>{icon}</div>;
+  }
+  let headingSection;
+  if (heading) {
+    headingSection = <p className={cx('heading')}>{heading}</p>;
+  }
+
+  let subtextSection;
+  if (subtext) {
+    subtextSection = <p className={cx('subtext')}>{subtext}</p>;
+  }
+
+  let subtextContentSection;
+  if (subtextContent) {
+    subtextContentSection = (<div className={cx('subtext-content')}>{subtextContent}</div>);
+  }
+
+  let divider;
+  if (subtextContent) {
+    divider = (<hr className={cx('divider')} style={{ margin: -1 }} />);
+  }
+
   const attributes = Object.assign({}, customProps);
   const StatusViewClassNames = cx([
     'status-view',
     attributes.className,
   ]);
 
-  return (<div {...attributes} className={StatusViewClassNames} />)
+  return (
+    <div {...customProps} className={StatusViewClassNames}>
+      {iconSection}
+      {headingSection}
+      {subtextSection}
+      {divider}
+      {subtextContentSection}
+    </div>
+  );
 };
 
 StatusView.propTypes = propTypes;
