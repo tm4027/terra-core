bff8c9ac6654c954d905e1e41548eddec81f975e
AF042410 <altamash.fakki@cerner.com>
2017-09-13 17:57:53 -0500

    updating theming logic and removing unused variables and mixins files

--- packages/terra-site/src/examples/status-view/ClinicalStatusView.jsx
+++ packages/terra-site/src/examples/status-view/ClinicalStatusView.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import StatusView from 'terra-status-view';
+import ThemeProvider from 'terra-theme-provider';
 
 const subtextContent = (<p>Buttons or other controls comes here for next action</p>);
+const theme = 'cerner-consumer-theme';
+
 const ClinicalStatusView = () => (
   <div>
-    <StatusView
-      type="clinical"
-      subtext="Description comes here"
-      subtextContent={subtextContent}
-    />
+    <ThemeProvider themeName={theme}>
+      <StatusView
+        type="clinical"
+        subtext="Description comes here"
+        subtextContent={subtextContent}
+      />
+    </ThemeProvider>
   </div>
 );
 
