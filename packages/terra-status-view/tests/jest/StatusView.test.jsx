cb4dcd37c4af4e96c6322f0e0a9052ddb769fd78
AF042410 <altamash.fakki@cerner.com>
2017-09-15 17:06:41 -0500

    add snapshot test

--- packages/terra-status-view/tests/jest/StatusView.test.jsx
+++ packages/terra-status-view/tests/jest/StatusView.test.jsx
@@ -1,12 +1,113 @@
 import React from 'react';
+import IconClose from 'terra-icon/lib/icon/IconClose';// TODO
 import StatusView from '../../src/StatusView';
 
 describe('StatusView', () => {
-  const defaultRender = <StatusView type="custom" />;
+  const subtextContent = (<p>Buttons or other controls comes here for next action</p>);
 
   // Snapshot Tests
-  it('should render a default component', () => {
-    const wrapper = shallow(defaultRender);
+  it('should render a custom status view component', () => {
+    const custom = (<StatusView
+      icon={<IconClose />}
+      heading="custom Heading"
+      type="custom"
+      subtext="Description comes here"
+      subtextContent={subtextContent}
+    />);
+    const wrapper = shallow(custom);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a custom status view component without icon', () => {
+    const custom = (<StatusView
+      heading="custom Heading"
+      type="custom"
+      subtext="Description comes here"
+      subtextContent={subtextContent}
+      isIconHidden
+    />);
+    const wrapper = shallow(custom);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a no_data status view component', () => {
+    const testComp = (<StatusView
+      type="no_data"
+      subtext="Description comes here"
+      subtextContent={subtextContent}
+    />);
+    const wrapper = shallow(testComp);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a no_data status view component without icon', () => {
+    const testComp = (<StatusView
+      type="no_data"
+      subtext="Description comes here"
+      subtextContent={subtextContent}
+      isIconHidden
+    />);
+    const wrapper = shallow(testComp);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a no_result status view component', () => {
+    const testComp = (<StatusView
+      type="no_result"
+      subtext="Description comes here"
+      subtextContent={subtextContent}
+    />);
+    const wrapper = shallow(testComp);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a not_authorized status view component', () => {
+    const testComp = (<StatusView
+      type="not_authorized"
+      subtext="Description comes here"
+      subtextContent={subtextContent}
+    />);
+    const wrapper = shallow(testComp);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a sensitive_data status view component', () => {
+    const testComp = (<StatusView
+      type="sensitive_data"
+      subtext="Description comes here"
+      subtextContent={subtextContent}
+    />);
+    const wrapper = shallow(testComp);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a error status view component', () => {
+    const testComp = (<StatusView
+      type="error"
+      subtext="Description comes here"
+      subtextContent={subtextContent}
+    />);
+    const wrapper = shallow(testComp);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a no_internet status view component', () => {
+    const testComp = (<StatusView
+      type="no_internet"
+      subtext="Description comes here"
+      subtextContent={subtextContent}
+    />);
+    const wrapper = shallow(testComp);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a error_loading status view component', () => {
+    const testComp = (<StatusView
+      type="error_loading"
+      subtext="Description comes here"
+      subtextContent={subtextContent}
+    />);
+    const wrapper = shallow(testComp);
     expect(wrapper).toMatchSnapshot();
   });
 });
